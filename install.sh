366d32d05bd1b46081f0c0b97c34b1dc8e383a76
Marco Scheidhuber <j23d@jusid.de>
2012-10-19 17:03:59 +0200

    Working install script.

--- install.sh
+++ install.sh
@@ -16,18 +16,63 @@ while [ $# -gt 0 ]; do
             USERNAME="$2"
             shift
             ;;
+        -sk|--SESSIONKEY)
+            SESSIONKEY="$2"
+            shift
+            ;;
     esac
     shift
 done
 
+
+# catch the variables
 # if [[ -z $DATABASE && -z $USERNAME ]]; then
 #     error
 # fi
 
+if [ -z $DATABASE ]; then
+    DATABASE='sqlite:\/\/\/%(here)s\/Kotti.db'
+fi
+
+if [[ -z $USERNAME ]]; then
+    dir="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+    OIFS=$IFS
+    IFS='/'
+    paths=($dir)
+    IFS=$OIFS
+    USERNAME=${paths[4]}
+fi
+
+SESSIONKEY="$USERNAME-kotti"
+SESSIONSECRET="$USERNAME-SECRET-0815"
+MAILDEFAULTSENDER="admin@something.tld"
+MAILUSERNAME="mustermann@something.tld"
+MAILPASSWORD=""
+KOTTISITETITLE="Kotti"
+KOTTISECRET="qwerty"
+
+
+echo "Prepare the buildout file"
+cp ./config/buildout.cfg.in ./buildout.cfg
+sed -i "s/%databasestring%/$DATABASE/g" ./buildout.cfg
+sed -i "s/%username%/$USERNAME/g" ./buildout.cfg
+sed -i "s/%session_key%/$SESSIONKEY/g" ./buildout.cfg
+sed -i "s/%session_secret%/$SESSIONSECRET/g" ./buildout.cfg
+sed -i "s/%mail_default_sender%/$MAILDEFAULTSENDER/g" ./buildout.cfg
+sed -i "s/%mail_username%/$MAILUSERNAME/g" ./buildout.cfg
+sed -i "s/%mail_password%/$MAILPASSWORD/g" ./buildout.cfg
+sed -i "s/%kotti_site_title%/$KOTTISITETITLE/g" ./buildout.cfg
+sed -i "s/%kotti_secret%/$KOTTISECRET/g" ./buildout.cfg
+
+
 # create virtualenv, activate it and run buildout
-virtualenv-2.7 .;
-source ./bin/activate;
-python bootstrap.py
+if [ ! -f ./bin/python2.7 ]; then
+    echo "Installing virtualenv."
+    virtualenv-2.7 --distribute .
+fi
+
+echo "Running the buildout."
+./bin/python2.7 bootstrap.py
 ./bin/buildout
 
-#sed 's|.*\/\([^\.]*\)\(\..*\)$|\1|g'
+exit 0;
